3af338915c92c1c8d05c7fbe44b040b858724a3f
Mhmod <mahmoud97787@gmail.com>
2024-09-15 16:31:50 +0300

    create show modal Bottom sheet in note view

---
+++ lib/views/note_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/body_of_bottom_sheet.dart';
+
+
+class NoteView extends StatelessWidget {
+  const NoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { 
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (builder) {
+              return const BodyOfBottomSheet();
+            },
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      
+    );
+  }
+}
