df9dea0cfc96028e129fead325a1096e27a3ff69
Mhmod <mahmoud97787@gmail.com>
2024-09-15 16:22:21 +0300

    create constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const String kBoxNotes = 'notes_app';
+const List<Color> kColorPalette = [
+  Color(0xff65655E),
+  Color(0xff7D80DA),
+  Color(0xffB0A3D4),
+  Color(0xffCEBACF),
+  Color(0xffC6AFB1),
+  Color(0xffFF4365),
+];
